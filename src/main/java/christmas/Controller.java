d4ead613c82ceef31578051edd1a2d9a49debc71
junghunim07 <dlawndgns9@naver.com>
2023-11-10 15:55:23 +0900

    feat: 메뉴 입력 받기 기능 추가
    + 예외 처리

--- src/main/java/christmas/Controller.java
+++ src/main/java/christmas/Controller.java
@@ -2,8 +2,11 @@ package christmas;
 
 import christmas.ui.InputView;
 import christmas.ui.OutputView;
+import java.util.HashMap;
+import java.util.List;
 
 public class Controller {
+    HashMap<String, Integer> menuBoard = new HashMap<>();
     InputView input = new InputView();
     OutputView output = new OutputView();
 
@@ -14,9 +17,38 @@ public class Controller {
         inputValueValidation(date);
     }
 
+    public void getMenuStart() {
+        output.notifyGetMenu();
+        List<String> menus = input.getMenu();
+        transform(menus);
+        System.out.print(menuBoard);
+    }
+
     public void inputValueValidation(int date) {
         if (date < 1 || date > 31) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public void transform(List<String> menus) {
+        for (int i = 0; i < menus.size(); i += 2) {
+            menuBoard.put(menuDuplicateValidation(menus.get(i)), menuCountValidation(menus.get(i + 1)));
+        }
+    }
+
+    public String menuDuplicateValidation(String menu) {
+        if (menuBoard.containsKey(menu)) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+        return menu;
+    }
+
+    public int menuCountValidation(String number) {
+        try {
+            int count = Integer.parseInt(number);
+            return count;
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
--- src/main/java/christmas/ui/InputView.java
+++ src/main/java/christmas/ui/InputView.java
@@ -1,6 +1,8 @@
 package christmas.ui;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputView {
     public int getVisitDate() {
@@ -9,6 +11,19 @@ public class InputView {
             return date;
         }catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    public List<String> getMenu() {
+        List<String> input = new ArrayList<>();
+        try {
+            String[] menus = Console.readLine().split("-|,");
+            for (int i = 0 ; i < menus.length; i++) {
+                input.add(menus[i]);
+            }
+            return input;
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 }
