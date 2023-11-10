ad7763977a19bc5f5feeae3d305487f3fda5b416
junghunim07 <dlawndgns9@naver.com>
2023-11-10 16:06:05 +0900

    test: 메뉴 개수 예외 테스트

--- src/test/java/christmas/ControllerTest.java
+++ src/test/java/christmas/ControllerTest.java
@@ -1,21 +1,32 @@
 package christmas;
 
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ControllerTest {
+    Controller controller;
+    @BeforeEach
+    void setUp() {
+        controller = new Controller();
+    }
 
     @DisplayName("1 ~ 31사이의 숫자가 아닐 경우 예외 테스트")
     @ParameterizedTest(name = "{displayName} ({0}원)")
     @ValueSource(ints =  {0, 32})
     void inputValueException(int date) {
-        Controller controller = new Controller();
         assertThatThrownBy(() -> controller.inputValueValidation(date)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴판 예외 테스트")
+    @Test
+    void menuBoardException() {
+        assertThatThrownBy(() -> controller.transform(List.of("해산물파스타", "2","초코케이크", "1", "레드와인", "a")))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
