a326d6db73c0d32b9464c737463808b3e8da97cf
junghunim07 <dlawndgns9@naver.com>
2023-11-11 18:52:53 +0900

    feat: 이벤트 할인 총합 및 주문 가격 총합 기능

--- src/main/java/christmas/Controller.java
+++ src/main/java/christmas/Controller.java
@@ -1,6 +1,7 @@
 package christmas;
 
 import christmas.domain.Calculator;
+import christmas.domain.EventChecker;
 import christmas.domain.MenuMachine;
 import christmas.ui.InputView;
 import christmas.ui.OutputView;
@@ -11,6 +12,7 @@ public class Controller {
     InputView input = new InputView();
     OutputView output = new OutputView();
     Calculator calculator = new Calculator();
+    EventChecker eventChecker = new EventChecker();
 
     public void christmasPlannerStart() {
         output.notifyExplanation();
@@ -24,6 +26,26 @@ public class Controller {
         List<String> menus = input.getMenu();
         MenuMachine.transform(menus);
         output.notifyOrderMenu(MenuMachine.menuBoard);
+    }
+
+    public int getTotalDiscount() {
+        int totalDiscount = 0;
+        if (eventChecker.checkChristmasEventPeriod(date)) {
+            totalDiscount += eventChecker.christmasEvent(date);
+        }
+        if (!eventChecker.checkWeekdayOrWeekend(date)) {
+            totalDiscount += eventChecker.weekdayEvent();
+        }
+        if (eventChecker.checkWeekdayOrWeekend(date)) {
+            totalDiscount += eventChecker.weekendEvent();
+        }
+        if (eventChecker.checkSpecialEventPeriod(date)) {
+            totalDiscount += eventChecker.specialEvent();
+        }
+        if (eventChecker.checkGiftEvent(getTotalPrice())) {
+            totalDiscount += eventChecker.giveGiftEvent();
+        }
+        return totalDiscount;
     }
 
     public int getTotalPrice() {
