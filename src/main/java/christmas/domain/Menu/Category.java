f950ca80f258883b74b660d2978ab65b57fca066
junghunim07 <dlawndgns9@naver.com>
2023-11-13 09:06:46 +0900

    refactor: category 외부 클래스로 생성

---
+++ src/main/java/christmas/domain/Menu/Category.java
@@ -0,0 +1,18 @@
+package christmas.domain.Menu;
+
+enum Category {
+    APPETIZER("애피타이저"),
+    MAIN("메인"),
+    DESSERT("디저트"),
+    BEVERAGE("음료");
+
+    private final String category;
+
+    Category(String category) {
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+}
