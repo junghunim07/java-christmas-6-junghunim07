066bbc386605a5d7d80302ea802a480b5bec092d
junghunim07 <dlawndgns9@naver.com>
2023-11-13 11:32:34 +0900

    test: 크리스마스 이벤트 날이 아닐 경우 테스트

--- src/test/java/christmas/domain/Event/ChristmasEventTest.java
+++ src/test/java/christmas/domain/Event/ChristmasEventTest.java
@@ -1,18 +1,31 @@
 package christmas.domain.Event;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class ChristmasEventTest {
+    ChristmasEvent christmasEvent;
+
+    @BeforeEach
+    void setUp() {
+        christmasEvent = new ChristmasEvent("크리스마스 디데이 할인", 0);
+    }
 
     @DisplayName("크리스마스 할인 가격 테스트")
     @ParameterizedTest(name = "{displayName} ({0}일)")
     @CsvSource({"13, 2200", "20, 2900"})
     void getChristmasEvent(int date, int expectedDiscount) {
-        ChristmasEvent christmasEvent = new ChristmasEvent("크리스마스 디데이 할인", 0);
         assertThat(christmasEvent.getChristmasEvent(date)).isEqualTo(expectedDiscount);
+    }
+
+    @DisplayName("크리스마스 이벤트 날이 아닐 경우 테스트")
+    @Test
+    void getChristmasNotEvent() {
+        assertThat(christmasEvent.getChristmasEvent(27)).isEqualTo(0);
     }
 }
