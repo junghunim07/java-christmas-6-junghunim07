d6e1822df2b3979ec723769bc5fc0c72389e87fb
junghunim07 <dlawndgns9@naver.com>
2023-11-13 22:47:03 +0900

    test: 총액이 만원이 넘지 않을 경우 테스트

--- src/test/java/christmas/domain/Event/SpecialEventTest.java
+++ src/test/java/christmas/domain/Event/SpecialEventTest.java
@@ -3,16 +3,28 @@ package christmas.domain.Event;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class SpecialEventTest {
+    SpecialEvent specialEvent;
+
+    @BeforeEach
+    void setUp() {
+        specialEvent = new SpecialEvent("특별 할인", 0);
+    }
 
     @DisplayName("특별 할인 금액 테스트")
     @Test
     void getSpecialEventDiscount() {
-        SpecialEvent specialEvent = new SpecialEvent("특별 할인", 0);
         int expectedResult = 1000;
-        assertThat(specialEvent.getSpecialEventDiscount(3)).isEqualTo(expectedResult);
+        assertThat(specialEvent.getSpecialEventDiscount(3, 20000)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("총액이 만원을 넘지 않을 경우 테스트")
+    @Test
+    void lowTotalPriceAmountAboutSpecialEventTest() {
+        assertThat(specialEvent.getSpecialEventDiscount(3, 5000)).isEqualTo(0);
     }
 }
--- src/test/java/christmas/domain/Event/WeekdayEventTest.java
+++ src/test/java/christmas/domain/Event/WeekdayEventTest.java
@@ -1,17 +1,29 @@
 package christmas.domain.Event;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class WeekdayEventTest {
+    WeekdayEvent weekdayEvent;
+
+    @BeforeEach
+    void setUp() {
+        weekdayEvent = new WeekdayEvent("평일 할인", 0);
+    }
 
     @DisplayName("평일 할인 금액 계산 테스트")
     @Test
     void getWeekdayDiscount() {
-        WeekdayEvent weekdayEvent = new WeekdayEvent("평일 할인", 0);
         int expectedResult = 6069;
-        assertThat(weekdayEvent.getWeekdayDiscount(7, 3)).isEqualTo(expectedResult);
+        assertThat(weekdayEvent.getWeekdayDiscount(7, 3, 20000)).isEqualTo(expectedResult);
+    }
+
+    @DisplayName("총액이 만원을 넘지 않을 경우 테스트")
+    @Test
+    void lowTotalPriceAmountAboutWeekdayEvent() {
+        assertThat(weekdayEvent.getWeekdayDiscount(7, 3, 5000)).isEqualTo(0);
     }
 }
